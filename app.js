4d19fc15445e1e7499777e254997a5024cbc107c
Abhinav kulkarni <abhinavmkulkarni@gmail.com>
2014-09-28 21:55:13 +0530

    not working

--- app.js
+++ app.js
@@ -87,10 +87,9 @@ app.post('/projects/get',function(req, res){
 /**
  * Start Server
  */
-
-// http.createServer(app).listen(app.get('port'), function () {
-//   console.log('Express server listening on port ' + app.get('port'));
-// });
+ http.createServer(app).listen(app.get('port'), function () {
+   console.log('Express server listening on port ' + app.get('port'));
+ });
 
 // functions
 // generate accesstoken
