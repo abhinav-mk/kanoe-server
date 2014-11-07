d2feb6ecd1e699dc445036c39f5aac511887f9a4
Abhinav kulkarni <abhinavmkulkarni@gmail.com>
2014-11-08 00:19:27 +0530

    testing people

--- app.js
+++ app.js
@@ -246,8 +246,16 @@ app.get('/people/image/get/:name',function(req, res){
   var temp_name = '/tmp/'+id+'.png'
   var getimage = getPeopleImage(id);
   getimage.then(function(data){
-    console.log("data:",data)
-    fs.writeFile(temp_name, data.rows[0]);
+    var i;
+    for(i=0;i<data.rows.length;i++)
+    {
+      if(data.rows[i].id == id)
+      {
+        break;
+      }
+    }
+    console.log("data:",data.rows[i])
+    fs.writeFile(temp_name, data.rows[i].image);
     res.send(fs.createWriteStream(temp_name))
   });
 });
@@ -654,7 +662,7 @@ var getPeopleImageId = function(){
 var getPeopleImage = function(id){
   var deferred = q.defer();
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-      client.query('SELECT * FROM people_images where id ='+id+'', function(err, result) {
+      client.query('SELECT * FROM people_images', function(err, result) {
           done();
           if (err)
           { 
